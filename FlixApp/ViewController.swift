49cc28bbe6be67d37a2517b463d871bb38cfdec0
Xavier Loera Flores <x.loera.tech@gmail.com>
2020-09-05 20:03:18 -0700

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Xavier Loera Flores on 9/5/20.
+//  Copyright © 2020 Xavier Loera Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
